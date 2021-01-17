60ea2705b6e26abdf509184f12005f7294e24ad5
aquiles <aquiles.viza@sansano.usm.cl>
2021-01-16 22:49:30 -0300

    Limpié un poco, eliminé redis lo msá que pude

--- codebase/server.py
+++ codebase/server.py
@@ -1,26 +1,13 @@
 from globals import showKeys, setCAKeys
-import sys
 from aiohttp import web
-import redis
 from routes.index import index_router
 from routes.certificates import cert_router
 from routes.key_exchange import exchange_router
 
 
-def setKeys():
-    redis_conn = redis.StrictRedis(host="localhost")
-
-    with open("keys/ca_priv_key_ECC.pem", "rb") as f:
-        redis_conn.set(b"ca_priv_key", f.read())
-
-    with open("keys/ca_pub_key_ECC.pem", "rb") as f:
-        redis_conn.set(b"ca_pub_key", f.read())
-
-
 async def make_app():
     app = web.Application()
 
-    # setKeys()
     setCAKeys()
     showKeys()
 
@@ -38,4 +25,4 @@ if __name__ == "__main__":
 
     except Exception as e:
         print(f"Error: Aplicación falló")
-        sys.exit(-1)
+        exit(-1)
